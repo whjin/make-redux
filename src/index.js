fd10c6d4cc16e8230b20c801a0973bf49bb8f838
whjin <1414848321@qq.com>
2019-02-13 03:09:18 +0800

    :star:更新文档

--- src/index.js
+++ src/index.js
@@ -1,96 +1,59 @@
-const appState = {
-    title: {
-        text: 'React.js title',
-        color: 'red'
-    },
-    content: {
-        text: 'React.js content',
-        color: 'blue'
-    }
-};
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import Header from './Header';
+import Content from './Content';
+import './index.css';
 
-const newAppState = {
-    ...appState,
-    title: {
-        ...appState,
-        text: 'React.js'
-    }
-};
-const newAppState1 = {
-    ...newAppState,
-    title: {
-        ...newAppState.title,
-        color: 'blue'
-    }
-};
-
-function stateChanger(state, action) {
-    if(!state){
-        return {
-            title: {
-                text: 'React.js title',
-                color: 'red'
-            },
-            content: {
-                text: 'React.js content',
-                color: 'blue'
-            }
-        }
-    }
-    switch (action.type) {
-        case 'UPDATE_TITLE_TEXT':
-            return {
-                ...state,
-                title: {
-                    ...state.title,
-                    text: action.text
-                }
-            };
-        case 'UPDATE_TITLE_COLOR':
-            return {
-                ...state,
-                title: {
-                    ...state.title,
-                    color: action.color
-                }
-            };
-        default:
-            return state;
-    }
-}
-
-function renderApp(appState) {
-    renderTitle(appState.title);
-    renderContent(appState.content)
-}
-
-function renderTitle(title) {
-    const titleDOM = document.getElementById('title');
-    titleDOM.innerHTML = title.text;
-    titleDOM.style.color = title.color
-}
-
-function renderContent(content) {
-    const contentDOM = document.getElementById('content');
-    contentDOM.innerHTML = content.text;
-    contentDOM.style.color = content.color
-}
-
-function createStore(state, stateChanger) {
+function createStore(reducer) {
+    let state = null;
     const listeners = [];
     const subscribe = (listener) => listeners.push(listener);
     const getState = () => state;
     const dispatch = (action) => {
-        state = stateChanger(state, action);
-        listeners.forEach((listener) => listener())
+        state = reducer(state, action);
+        listeners.forEach((listener) => listener());
     };
-    return {getState, dispatch, subscribe}
+    dispatch({});
+    return {getState, dispatch, subscribe};
 }
 
+const themeReducer = (state, action) => {
+    if (!state) {
+        return {
+            themeColor: 'red'
+        }
+    }
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            return {...state, themeColor: action.themeColor};
+        default:
+            return state;
+    }
+};
 
-const store = createStore(appState, stateChanger);
-store.subscribe(() => renderApp(store.getState()));
+const store = createStore(themeReducer);
 
-renderApp(store.getState());
-store.dispatch({type: 'UPDATE_TITLE_TEXT', text: 'React.js'});
-store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'purple'});
+class Index extends Component {
+    static childContextTypes = {
+        store: PropTypes.object
+    };
+
+    getChildContext() {
+        return {store};
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Content/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index/>,
+    document.getElementById('root')
+);
